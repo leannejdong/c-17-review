3137794b1af8ecb70807f291b247d8c8464d3b73
Leanne J Dong <jdleanne@gmail.com>
2021-11-23 10:58:57 -0500

    add std::optional for stream.cpp

--- fileIO/stream.cpp
+++ fileIO/stream.cpp
@@ -3,25 +3,41 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <optional>
 
 using std::vector;
 using std::string;
-// C++11 version
-int main(){
+using std::ifstream;
 
-    std::ifstream ifile1("verona.csv");
-    if(ifile1){
-        std::cout << "File opened\n";
-        vector<vector<string>> data;
-        string line, word;
-        while (getline(ifile1, line)) {
-            std::stringstream ss(line);
-            vector<string> row;
-            while (getline(ss, word, ',')) {
-                row.push_back(word);
-            }
-            data.push_back(row);
-        }    
+inline std::optional<string> LoadFile(const std::string& file_path)
+{
+    ifstream stream(file_path);
+    if( stream )
+    {
+        std::string result;
+        return result;
+    }
+    return {};
+}
+
+int main() {
+  // std::optional require c++17
+  std::cerr << LoadFile("verona.csv")
+              .value_or("File [nw_verona.csv] could not be opened!")
+       << "\n";
+  ifstream ifile1("verona.csv");
+  if (ifile1) {
+    std::cout << "File opened\n";
+    vector<vector<string>> data;
+    string line, word;
+    while (getline(ifile1, line)) {
+      std::stringstream ss(line);
+      vector<string> row;
+      while (getline(ss, word, ',')) {
+        row.push_back(word);
+      }
+      data.push_back(row);
+    }    
     }
 }
 
